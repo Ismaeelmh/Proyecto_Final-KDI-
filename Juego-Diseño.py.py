91061782e96610de4e0728efb3e4f7bda468e74e
ismaeelmh <ismaeelmh75@gmail.com>
2026-04-09 17:20:59 +0200

    creado el botones del reinciar y de Salir

--- Juego-Diseño.py.py
+++ Juego-Diseño.py.py
@@ -6,7 +6,7 @@ import time
 pygame.init()
 
 # Configuración de la ventana
-WIDTH, HEIGHT = 600, 650
+WIDTH, HEIGHT = 680, 700
 ROWS, COLS = 8, 8
 CELL_SIZE = WIDTH // COLS
 
@@ -20,7 +20,14 @@ DARK_GRAY = (34, 139, 34)
 BLACK = (0, 100, 0)
 
 # Fuente
-font = pygame.font.SysFont("Arial", 30)
+font = pygame.font.SysFont("Arial", 25)
+button_font = pygame.font.SysFont("Arial", 25)
+
+
+# Botones del Reiniciar y Salir
+reinciar_botton = pygame.Rect(520, 12, 110, 35)
+salir_botton = pygame.Rect(610, 12, 90, 35)
+
 
 # Data del Juego 
 score = 0
@@ -49,11 +56,23 @@ def draw_ui():
     name_text = font.render(f"Jugador: {player_name}", True, WHITE)
     time_text = font.render(f"Tiempo: {elapsed_time}s", True, WHITE)
     score_text = font.render(f"Puntos: {score}", True, WHITE)
+    
+    # Dibujar Botones
+    pygame.draw.rect(screen, BLACK, reinciar_botton)
+    pygame.draw.rect(screen, BLACK, salir_botton)
+
+    # Texto de botones
+    reinciar_text = button_font.render("Reiniciar", True, WHITE)
+    salir_text = button_font.render("Salir", True, WHITE)
+
+    screen.blit(reinciar_text, (reinciar_botton.x + 5, reinciar_botton.y + 5))
+    screen.blit(salir_text, (salir_botton.x + 15, salir_botton.y + 5))
 
     # Posiciones
-    screen.blit(name_text, (10, 10))
-    screen.blit(time_text, (260, 10))
-    screen.blit(score_text, (420, 10))
+    screen.blit(name_text, (6, 10))   
+    screen.blit(time_text, (253, 10))
+    screen.blit(score_text, (398, 10))
+
 
 def new_func(ui_rect):
     pygame.draw.rect(screen, DARK_GRAY, ui_rect)
@@ -63,11 +82,18 @@ def draw_grid():
     for row in range(ROWS):
         for col in range(COLS):
             x = col * CELL_SIZE
-            y = row * CELL_SIZE + 50  # espacio para la barra
+            y = row * CELL_SIZE + 50
 
             rect = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
             pygame.draw.rect(screen, WHITE, rect)
             pygame.draw.rect(screen, BLACK, rect, 2)
+
+
+# Función para reiniciar
+def reset_game():
+    global score, start_time
+    score = 0
+    start_time = time.time()
 
 
 # Bucle principal
@@ -79,7 +105,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Actualiaz el Tiempo
+        # Detectar clics
+        if event.type == pygame.MOUSEBUTTONDOWN:  
+            mouse_pos = pygame.mouse.get_pos()
+
+            if reinciar_botton.collidepoint(mouse_pos):
+                reset_game()
+                print("Reiniciado")
+
+            if salir_botton.collidepoint(mouse_pos):
+                running = False
+
+    # Actualiza el Tiempo
     elapsed_time = int(time.time() - start_time)
 
     draw_ui()
